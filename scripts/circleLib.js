abcdd0cb48e659506f7187a8993c520c5281ec35
Thomas Tursics <thomas@tursics.de>
2015-06-16 22:38:46 +0200

    added a button shape
    added a complex radial gradient for a button layout

--- scripts/circleLib.js
+++ scripts/circleLib.js
@@ -6,6 +6,7 @@ function CircleLib( elementID)
 {
 	this.element = null;
 	this.clickCallback = null;
+	this.fontSize = 1;
 
 	try {
 		this.element = document.getElementById(elementID);
@@ -33,7 +34,8 @@ CircleLib.prototype.init = function()
 	this.OnResize();
 
 	this.element.style.position = 'absolute';
-	this.element.style.background = 'rgba(0,0,0,.3)';
+//	this.element.style.background = 'rgba(0,0,0,.3)';
+	this.element.style.color = '#ffffff';
 	this.element.style.zIndex = '10';
 	this.element.style.textAlign = 'center';
 	this.element.style.webkitTouchCallout = 'none';
@@ -56,11 +58,13 @@ CircleLib.prototype.rotate = function(degree)
 
 CircleLib.prototype.text = function(str,fontSize)
 {
+	this.fontSize = fontSize;
+
 	var elementHeight = parseInt(this.element.style.height);
-	var lineHeight = parseInt(elementHeight/10*fontSize);
+	var fontSizeHeight = parseInt(elementHeight/10*this.fontSize);
 
 	this.element.innerHTML = str;
-	this.element.style.fontSize = lineHeight+'px';
+	this.element.style.fontSize = fontSizeHeight+'px';
 }
 
 // ---------------------------------------------------------------------------------------
@@ -78,6 +82,9 @@ CircleLib.prototype.OnResize = function()
 	var x = window.innerWidth  || document.documentElement.clientWidth  || body.clientWidth;
 	var y = window.innerHeight || document.documentElement.clientHeight || body.clientHeight;
 	var min = x > y ? y : x;
+	min *= .85;
+
+	var fontSizeHeight = parseInt(min/10*this.fontSize);
 
 	this.element.style.top = ((y-min)/2)+'px';
 	this.element.style.left = ((x-min)/2)+'px';
@@ -87,6 +94,42 @@ CircleLib.prototype.OnResize = function()
 	this.element.style.webkitBorderRadius = min+'px';
 	this.element.style.MozBorderRadius = min+'px';
 	this.element.style.borderRadius = min+'px';
+	this.element.style.fontSize = fontSizeHeight+'px';
+
+	this.setGradient();
+}
+
+// ---------------------------------------------------------------------------------------
+
+CircleLib.prototype.setGradient = function()
+{
+	var innerColor = 'rgba(0,0,0,0)';
+	var borderColor = 'rgba(0,0,0,.2)';
+	var outerColor = 'rgba(0,0,0,.05)';
+	var frameColor = 'rgba(255,255,255,.1)';
+	var innerPos = '54.5%';
+	var borderPos = '55%';
+	var outerPos = '61%';
+	var frame1Pos = '65.5%';
+	var frame2Pos = '66%';
+
+	// old browsers
+	this.element.style.background = innerColor;
+
+	// firefox 3.6+
+	this.element.style.background = '-moz-radial-gradient(center, ellipse cover,'+innerColor+' 0%,'+innerColor+' '+innerPos+','+borderColor+' '+borderPos+','+outerColor+' '+outerPos+','+outerColor+' '+frame1Pos+','+frameColor+' '+frame2Pos+','+frameColor+' 100%)';
+
+	// chrome10+ and safari5.1+
+	this.element.style.background = '-webkit-radial-gradient(center, ellipse cover,'+innerColor+' 0%,'+innerColor+' '+innerPos+','+borderColor+' '+borderPos+','+outerColor+' '+outerPos+','+outerColor+' '+frame1Pos+','+frameColor+' '+frame2Pos+','+frameColor+' 100%)';
+
+	// opera 12+
+	this.element.style.background = '-o-radial-gradient(center, ellipse cover,'+innerColor+' 0%,'+innerColor+' '+innerPos+','+borderColor+' '+borderPos+','+outerColor+' '+outerPos+','+outerColor+' '+frame1Pos+','+frameColor+' '+frame2Pos+','+frameColor+' 100%)';
+
+	// ie10+
+	this.element.style.background = '-ms-radial-gradient(center, ellipse cover,'+innerColor+' 0%,'+innerColor+' '+innerPos+','+borderColor+' '+borderPos+','+outerColor+' '+outerPos+','+outerColor+' '+frame1Pos+','+frameColor+' '+frame2Pos+','+frameColor+' 100%)';
+
+	// modern css
+	this.element.style.background = 'radial-gradient(ellipse at center,'+innerColor+' 0%,'+innerColor+' '+innerPos+','+borderColor+' '+borderPos+','+outerColor+' '+outerPos+','+outerColor+' '+frame1Pos+','+frameColor+' '+frame2Pos+','+frameColor+' 100%)';
 }
 
 // ---------------------------------------------------------------------------------------
